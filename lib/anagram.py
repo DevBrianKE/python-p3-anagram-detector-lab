bea5e83b9abbe62b4fe13b8633d360ce108678ab
DevBrianKE <kipchumbabrian47@gmail.com>
2025-05-14 09:40:27 +0300

    Implement Anagram Detector

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,13 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, word_list):
+        sorted_original = sorted(self.word.lower())
+        result = []
+
+        for w in word_list:
+            if sorted(w.lower()) == sorted_original and w.lower() != self.word.lower():
+                result.append(w)
+        return result
